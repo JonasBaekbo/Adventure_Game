6d9cda0084ebcef4537c7348fd93c3f4bf569344
Bruger <mikk00j3@stud.kea.dk>
2021-10-13 13:34:34 +0200

    more items and enemys

--- src/com/company/Map.java
+++ src/com/company/Map.java
@@ -32,10 +32,13 @@ public class Map {
         room9.setNorth(room6);
         room9.setWest(room8);
 
+        room1.setListOfItems(new MeleeWeapon("sword", "a short sword", 2));
         room1.setListOfItems(new Item("key", "A key to a small house"));
         room1.setListOfItems(new Food("apple", "A shiny apple", 2));
-        room1.setListOfItems(new MeleeWeapon("axe", "a rust axe",3));
-        room1.setListOfItems(new RangedWeapon("musket", "a old musket" ,3 ,8));
+        room8.setListOfItems(new Food("can", "A can of food", 5));
+        room9.setListOfItems(new Food("corncob", "a cooked corncob", 3));
+        room4.setListOfItems(new MeleeWeapon("axe", "a rust axe",3));
+        room8.setListOfItems(new RangedWeapon("musket", "a old musket" ,3 ,8));
         room2.setListOfItems(new Item("rock", "A little black shiny rock"));
         room3.setListOfItems(new Item("branch", "A small branch that has fallen down from a tree"));
         room5.setListOfItems(new Item("wine", "A tasty old wine from the ancient days"));
@@ -44,7 +47,9 @@ public class Map {
         room9.setListOfItems(new Item("Pen", "A used pen"));
         room9.setListOfItems(new Item("Doll", "A creepy looking doll, with giant eyes"));
         room9.setListOfItems(new Item("Knife", "A rusty knife"));
-        room1.addEnemyToRoom(new Enemy("Ogre", "A big green ogre", 5, (new MeleeWeapon("Mace", "A wooden mace", 2))));
+        room7.addEnemyToRoom(new Enemy("Ogre", "A big green ogre", 5, (new MeleeWeapon("Mace", "A wooden mace", 2))));
+        room5.addEnemyToRoom(new Enemy("Gobli", "A tiny Gobli", 2 , (new MeleeWeapon("knife", "A tiny rysty knif", 1))));
+        room2.addEnemyToRoom(new Enemy("Troll", "A Giant forstes troll", 8 , (new MeleeWeapon("club" , "A giant wooden club", 4))));
 
 currentRoom = room1;
 
